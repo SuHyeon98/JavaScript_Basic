487bb84a7f17ff4f6c4b1757bcba7f163dd62b3e
SuHyeon98 <114630366+SuHyeon98@users.noreply.github.com>
2024-07-22 21:51:14 +0900

    Add files via upload

--- day07/array_advance/index.js
+++ day07/array_advance/index.js
@@ -1,49 +1,49 @@
-// // arrow function 축약
-// const even = (x) => x % 2 == 0;
-// const isLength = (x) => x.length == 5;
+// arrow function 축약
+const even = (x) => x % 2 == 0;
+const isLength = (x) => x.length == 5;
 
-// // array forEach()
-// // quiz 1번
-// const icecream = ["mint", "choco", "banana"];
-// icecream.forEach((x) => console.log(x.toUpperCase()));
+// array forEach()
+// quiz 1번
+const icecream = ["mint", "choco", "banana"];
+icecream.forEach((x) => console.log(x.toUpperCase()));
 
-// // some() == || , every() == &&
-// // quiz 2번
-// const result = icecream.some((x) => x.includes("a"));
-// console.log(result);
+// some() == || , every() == &&
+// quiz 2번
+const result = icecream.some((x) => x.includes("a"));
+console.log(result);
 
-// // quiz
-// const fruits = [
-//   "apple",
-//   "peach",
-//   "tomato",
-//   "plum",
-//   "pear",
-//   "durian",
-//   "grape",
-//   "lemon",
-// ];
+// quiz
+const fruits = [
+  "apple",
+  "peach",
+  "tomato",
+  "plum",
+  "pear",
+  "durian",
+  "grape",
+  "lemon",
+];
 
-// // 1번
-// const fourAndWordR = fruits.some((x) => x.length == 4 && x.includes("r"));
-// console.log(fourAndWordR);
+// 1번
+const fourAndWordR = fruits.some((x) => x.length == 4 && x.includes("r"));
+console.log(fourAndWordR);
 
-// // 2번
-// const oddAndWordP = fruits.some((x) => x.length % 2 && x.startsWith("p"));
-// console.log(oddAndWordP);
+// 2번
+const oddAndWordP = fruits.some((x) => x.length % 2 && x.startsWith("p"));
+console.log(oddAndWordP);
 
-// // 3번 모음으로 시작하는 단어가 있는가? - 합성함수
-// // 모음 a, e, i, o, u
-// const vowels = ["a", "e", "i", "o", "u"];
-// const gather = fruits.some((fruits) =>
-//   vowels.some((x) => fruits.startsWith(x))
-// );
-// console.log(gather);
+// 3번 모음으로 시작하는 단어가 있는가? - 합성함수
+// 모음 a, e, i, o, u
+const vowels = ["a", "e", "i", "o", "u"];
+const gather = fruits.some((fruits) =>
+  vowels.some((x) => fruits.startsWith(x))
+);
+console.log(gather);
 
-// // 4번 k, b, s 에서 하나라도 포함하는 단어가 있는가? - 합성함수
-// const kbs = ["k", "b", "s"];
-// const fruitsKbs = fruits.some((fruits) => kbs.some((x) => fruits.includes(x)));
-// console.log(fruitsKbs);
+// 4번 k, b, s 에서 하나라도 포함하는 단어가 있는가? - 합성함수
+const kbs = ["k", "b", "s"];
+const fruitsKbs = fruits.some((fruits) => kbs.some((x) => fruits.includes(x)));
+console.log(fruitsKbs);
 
 // 5번
 // 뉴스기사를 가져와, 입력한 단어가 포함되는지 물어보는 코드
