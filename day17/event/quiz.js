b6519dff8a3cdf47e281fe8cb9972e936b950f6e
SuHyeon98 <114630366+SuHyeon98@users.noreply.github.com>
2024-08-05 21:42:05 +0900

    Add files via upload

--- day17/event/quiz.js
+++ day17/event/quiz.js
@@ -1,32 +1,35 @@
 // 유저에게 품목과 가격을 입력 받고
-const item = prompt("품목, 가격").split(" ");
+const [item, price] = prompt("품목, 가격").split(" ");
 
 // 장바구비 프로그램 만들기
 
 const up = document.createElement("button");
+const num = document.createElement("span");
+const down = document.createElement("button");
+const buy = document.createElement("button");
+
 up.innerText = `+`;
+num.innerText = "0";
+num.style.padding = "5px";
+down.innerText = `-`;
+
 up.addEventListener("click", () => {
   num.innerText = Number(num.innerText) + 1;
-  num.innerText == 10 ? (num.style.color = "red") : num;
   num.innerText > 10 ? (num.innerText = Number(num.innerText) - 1) : num;
 });
 
-const num = document.createElement("span");
-num.innerText = "0";
-num.style.padding = "5px";
-
-const down = document.createElement("button");
-down.innerText = `-`;
 down.addEventListener("click", () => {
   num.innerText = Number(num.innerText) - 1;
-  num.innerText < 10 ? (num.style.color = "black") : num;
   num.innerText < 0 ? (num.innerText = Number(num.innerText) + 1) : num;
 });
 
-const buy = document.createElement("button");
 buy.innerText = "구매하기";
 buy.addEventListener("click", () => {
-  alert(` ${item[0]}값 : ${Number(num.innerText) * Number(item[1])}원`);
+  alert(
+    ` ${item} ${num.innerText}개 가격은 : ${
+      Number(num.innerText) * Number(price)
+    }원`
+  );
 });
 
 document.body.appendChild(up);
